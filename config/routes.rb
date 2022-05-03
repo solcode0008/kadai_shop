4067932bf260ec7923c5dc4384ffcf47c4237ee2
k49977 <r.085.tk@i.softbank.jp>
2022-05-03 10:37:32 +0900

    電話番号&マイページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
   resources :shops
+  get '/mypage', to: 'users#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_025637) do
+ActiveRecord::Schema.define(version: 2022_04_30_054759) do
 
   create_table "shops", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_04_30_025637) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
